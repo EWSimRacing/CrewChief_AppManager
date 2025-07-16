7448749f2747777f30650480eac8b304fd9f91ff
EWSimRacing <elliott.m.williams@outlook.com>
2025-07-15 20:33:56 -0400

    Update Uninstall.bat
    name change from Auto to App

--- Uninstall.bat
+++ Uninstall.bat
@@ -8,6 +8,6 @@ if %errorlevel% neq 0 (
 
 :: Run the PowerShell uninstall script
 SET script_dir=%~dp0
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%script_dir%Uninstall_CrewChief_AutoManager.ps1'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%script_dir%Uninstall_CrewChief_AppManager.ps1'"
 
 exit
